cd81fca29046e594de365ef9c89842570daef8fd
Witcher5566 <github77881@gmail.com>
2024-11-13 08:55:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ def is_enabled(value, default):
 # Bot information
 SESSION = environ.get('SESSION', 'N2Bot')
 API_ID = int(environ.get('API_ID', '23378704'))
-API_HASH = environ.get('API_HASH', '15a02b4d02babeb79e8f328b0ead0c17)
+API_HASH = environ.get('API_HASH', '15a02b4d02babeb79e8f328b0ead0c17')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
